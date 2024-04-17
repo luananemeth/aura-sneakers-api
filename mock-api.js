be473b3518a3ff6ab3fa8c575be1172f6a5ed481
Luana Nemeth <luana.nemeth@gmail.com>
2024-04-17 15:27:39 -0300

    feat: adds a mock api

---
+++ mock-api.js
@@ -0,0 +1,74 @@
+[
+  {
+    id: 1,
+    name: "Tênis Nike Air Force 1 07 Feminino",
+    price: 799.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/236413-760-760/Tenis-Nike-Air-Force-1-07-Feminino-Branco.jpg?v=637637032760830000",
+    brand: "nike",
+  },
+  {
+    id: 2,
+    name: "Tênis New Balance 9060",
+    price: 1500,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/479031-760-760/U60EE-B-600-1.jpg?v=638442271735400000",
+    brand: "new balance",
+  },
+  {
+    id: 3,
+    name: "Tênis adidas Ozweego Og W Feminino",
+    price: 799.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/463661-760-760/IE699-8-100-1.jpg?v=638339508206230000",
+    brand: "adidas",
+  },
+  {
+    id: 4,
+    name: "Tênis Puma Scoot 1 Cheetos Unissex",
+    price: 999.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/478692-760-760/30984-0-002-1.jpg?v=638440552917830000",
+    brand: "puma",
+  },
+  {
+    id: 5,
+    name: "Tênis Adidas Yeezy 500",
+    price: 1300,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/290901-760-760/GX360-6-200-1.jpg?v=638025829369170000",
+    brand: null,
+  },
+  {
+    id: 6,
+    name: "Tênis Puma Prevail",
+    price: 479.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/359386-1000-1000/38944-5-001-1.jpg?v=638137892115000000",
+    brand: null,
+  },
+  {
+    id: 7,
+    name: "Tênis Adidas Forum Bold Stripes Feminino",
+    price: 799.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/479152-1000-1000/IG397-2-400-1.jpg?v=638442272431530000",
+    brand: null,
+  },
+  {
+    id: 8,
+    name: "Tênis Nike W Cortez VNTG Feminino",
+    price: 699.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/475334-1000-1000/FJ253-0-300-1.jpg?v=638411188533800000",
+    brand: null,
+  },
+  {
+    id: 9,
+    name: "Tênis Adidas Rivalry Low W Feminino",
+    price: 699.99,
+    image:
+      "https://artwalk.vteximg.com.br/arquivos/ids/463591-1000-1000/ID756-0-100-1.jpg?v=638339494943430000",
+    brand: null,
+  },
+];
