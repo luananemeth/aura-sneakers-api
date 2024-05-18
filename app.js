6aec724248d624b4b44740978fae4214afda13dd
Luana Nemeth <luana.nemeth@gmail.com>
2024-05-18 18:45:37 -0300

    feat: creates /post customers and /post login

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const jwt = require("jsonwebtoken");
 
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 3000;
 
-var mysql = require("mysql");
+var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -30,6 +31,51 @@ app.get("/products", (request, response) => {
     function (error, results) {
       if (error) throw error;
       response.send(results);
+    }
+  );
+});
+
+app.post("/login", (request, response) => {
+  const { email, password } = request.body;
+
+  connection.query(
+    `SELECT * FROM customers WHERE email='${email}' and password='${password}'`,
+    function (error, results) {
+      if (error) throw error;
+
+      if (results.length === 0) {
+        return response.status(400).json({ errorCode: "INVALID_CREDENTIALS" });
+      }
+
+      response.send(results[0]);
+    }
+  );
+});
+
+app.post("/customers", (request, response) => {
+  const { name, password, email } = request.body;
+
+  connection.query(
+    `SELECT * FROM customers WHERE email LIKE '%${email}%'`,
+    function (error, results) {
+      if (error) throw error;
+
+      if (results.length > 0) {
+        return response.status(400).json({ errorCode: "USER_ALREADY_EXISTS" });
+      }
+
+      connection.query(`INSERT INTO customers (name, password, email) VALUES
+      (?,?,?)`, [name, password, email], (error,
+        results) => {
+        if (error) return response.json({ error: error }).sendStatus(500);
+
+        response.json({
+          id: results.insertId,
+          name,
+          password,
+          email,
+        });
+      });
     }
   );
 });
